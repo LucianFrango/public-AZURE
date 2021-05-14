1abf02f56d89d24f5e5eccf64cd7a448773f2238
Lucian Franghiu <lucian@outlook.com.au>
2021-05-14 12:38:30 +1000

    Remove files as these are moved to the /Lucian.Blog repo

--- 2019-06-14--Blog-096--Azure NSG security rule scripts/02.Remove.NSG.security.rules.from.CSV.ps1
+++
@@ -1,3 +0,0 @@
-$NSG = Get-AzNetworkSecurityGroup -Name <NetworkSecurityGroup> -ResourceGroupName <ResourceGroup>
-foreach($rule in import-csv "<C:\CSVFILEHERE.csv>"){$NSG | Remove-AzNetworkSecurityRuleConfig -Name $rule.name}
-$NSG | Set-AzNetworkSecurityGroup
